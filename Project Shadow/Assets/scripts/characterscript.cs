22a411b1c4fe03184e0bc2e1c55ffb675114ff2d
FlyingCow32 <gael.tournier32@gmail.com>
2024-11-07 08:34:47 +0100

    Created the basics
    - New character (without texture)
    - The character can move (if we can call that move)
    - There's a floor

---
+++ Project Shadow/Assets/scripts/characterscript.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class characterscript : MonoBehaviour
+{
+
+
+    public Rigidbody2D Charigidbody2D;
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W) == true) 
+        {
+            transform.position = transform.position + (Vector3.up * 1);
+        }
+        if (Input.GetKeyDown(KeyCode.A) == true)
+        { 
+            transform.position += (Vector3.left * 1);
+            Console.WriteLine("Hello2");
+        }
+        if (Input.GetKeyDown(KeyCode.D) == true)
+        {
+            transform.position += (Vector3.right * 1);
+            Console.WriteLine("Hello");
+        }        
+    }
+}
