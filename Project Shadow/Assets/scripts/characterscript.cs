19fb1216d223c441f577a3abe48cfc7f322c4584
FlyingCow32 <gael.tournier32@gmail.com>
2024-11-11 12:13:02 +0100

    Basic Dash system
    + Added dashing
    + Totally made with AI, everytime I try to explain the code with my own words so I can use it later
    
    Today's problem:
    My knowledge in methods and all that stuff is too poor for what I want to do, I must use AI to do things.
    What I learned with this commit:
    - IEnumerator : A return type that lets yield do its job
    - yield : a method that can run code and returns back where it left off
    - [Header] : To make a new section in Unity
    > is left is greater than right
    < is left is smaller than right
    fixedupdate : runs before update, mainly to fix physics

--- Project Shadow/Assets/scripts/characterscript.cs
+++ Project Shadow/Assets/scripts/characterscript.cs
@@ -12,12 +12,19 @@ public class characterscript : MonoBehaviour
     public LogicManager Logic;
     private bool isOnFloor = false;
     public float movementSpeed;
-    public float jumphight;
-  
+    public float jumpHeight;
+
+    [Header("Dash Settings")]
+    public float dashSpeed = 20f; //How fast the dash is
+    public float dashDuration = 1f; //How long each dash lasts
+    private bool isDashing = false;
+    //seting up is dashing to false by default
+    private float dashCooldown = 1f; // Time between dashes
+    private float lastDashTime = -3f; //Time since last dash
 
     void Start()
     {
-        Logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicManager>();
+        Logic = GameObject.FindWithTag("Logic").GetComponent<LogicManager>();
         Charigidbody2D = GetComponent<Rigidbody2D>();
     }
 
@@ -29,14 +36,35 @@ public class characterscript : MonoBehaviour
     
         if (Input.GetKeyDown("w") && isOnFloor)
         {
-            Charigidbody2D.velocity = new Vector2(Charigidbody2D.velocity.x, jumphight);
+            Charigidbody2D.velocity = new Vector2(Charigidbody2D.velocity.x, jumpHeight);
             Debug.Log("Jump");
         }
 
-        if (Input.GetKeyDown(KeyCode.LeftShift))
+        if (Input.GetKeyDown(KeyCode.LeftShift) && Time.time >= lastDashTime + dashCooldown)
         {
-            dash();
+            StartCoroutine(Dash());
+            Debug.Log("Dashing");
         }
+        // If the left shift key is pressed and the last time the chara. dash is less than -10 + 1 
+        // then start the coroutine (which pauses the game without pausing it and returns to the last frame)
+        if (isDashing == true)
+        {
+            Debug.Log("True");
+        }
+
+
+        // Character flip to fix dash direction
+        if (horizontalInput > 0 && transform.localScale.x < 0)
+        {
+            Flip();
+        }
+        // If the horizontal input is greater than 0 and the character is facing left, then flip the character to the right.
+        else if (horizontalInput < 0 && transform.localScale.x > 0)
+        {
+            Flip();
+        }
+        // If the horizontal input is less than 0 and chara is facing right (after 0 is right)
+        // then flip it to the left.
     }
 
 
@@ -55,8 +83,40 @@ public class characterscript : MonoBehaviour
         }
     }
 
-    public void dash()
-    {
-    
+    IEnumerator Dash()
+    { 
+        isDashing = true;
+        lastDashTime = Time.time; //Records the dash time 
+
+        float dashDirection = transform.localScale.x; //get the side of the character, if it's -1 then he is looking left, if 1 right. 
+        
+        Charigidbody2D.velocity = new Vector2(dashDirection * dashSpeed, Charigidbody2D.velocity.y);
+        yield return new WaitForSeconds(dashDuration);
+
+        isDashing = false;
     }
+    void FixedUpdate()
+    {
+        if (isDashing)
+        {
+            float dashDirection = transform.localScale.x;
+            Charigidbody2D.velocity = new Vector2(dashDirection * dashSpeed, Charigidbody2D.velocity.y);
+        }
+        //make this code run before update so it can be used in the update function
+    }
+
+    void Flip()
+    {
+        Vector3 characterScale = transform.localScale;
+        // get the local scale of the character
+        characterScale.x *= -1;
+        // flip the character
+        transform.localScale = characterScale;
+        //the chara's scale is now the flipped scale
+    }
+    
 }
+
+
+
+    
